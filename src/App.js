032824e8ea5b1165641c84ce084c5e73deeacf8d
Ramanan <ramanan@Ramanans-MacBook-Pro.local>
2021-06-09 16:57:33 +0530

    done

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there
+          Hello man
         </p>
         <a
           className="App-link"
